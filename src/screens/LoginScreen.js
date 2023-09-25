a8d83a6efc1997477fb5a41902693592c82ca3dd
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-09-26 01:53:28 +0530

    style - added logo in logi screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,13 +1,14 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View,Image } from 'react-native'
 import React from 'react'
-import { app } from '../firebase/firebaseConfig.js'
+import { auth ,db,storage} from '../firebase/firebaseConfig.js';
+import Logo from "../../assets/images/loginLogo.png"
 
 const LoginScreen = () => {
 
   return (
    <SafeAreaView style={styles.container}>
-      <View >
-        <Text>LOgin</Text>
+      <View style={styles.imageContainer}>
+       <Image source={Logo} style={styles.image}/>
       </View>
    </SafeAreaView>
   )
@@ -18,7 +19,17 @@ export default LoginScreen
 const styles = StyleSheet.create({
   container:{
     flex:1,
+    backgroundColor:"#fff"
+  },
+  imageContainer:{
+    flexDirection:"row",
     alignItems:"center",
-    justifyContent:"center"
+    justifyContent:"center",
+    marginTop:60,
+  },
+  image:{
+    height:200,
+    width:250,
+    resizeMode:"stretch"
   }
 })
