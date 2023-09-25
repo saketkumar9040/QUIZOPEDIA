cf35b64929a060f30c33d71acb480768d5c2982a
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-09-25 22:53:32 +0530

    feat- implemeted redux store and auth slice

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import "react-native-gesture-handler";
 import AuthNavigator from "./src/navigations/AuthNavigator";
+import { Provider } from "react-redux";
+import Store from "./src/redux/store";
 
 export default function App() {
-  return <AuthNavigator />;
+  return (
+    <Provider store={Store}>
+      <AuthNavigator />
+    </Provider>
+  );
 }
