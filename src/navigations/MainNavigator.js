423fe2172fa510e586d4f3b3b56e61d0dbcd67be
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-09-26 17:47:52 +0530

    style - added icons in bottom tabs

--- src/navigations/MainNavigator.js
+++ src/navigations/MainNavigator.js
@@ -4,6 +4,7 @@ import DashboardScreen from "../screens/dashboard";
 import ProfileScreen from "../screens/profile";
 import leaderBoardIcon from "../../assets/images/leaderboardIcon.png";
 import profileIcon from "../../assets/images/profile.png";
+import homeIcon from "../../assets/images/homeIcon.png"
 import { Image } from 'react-native';
 
 const Tab = createBottomTabNavigator();
@@ -24,9 +25,8 @@ const MainNavigator = () => {
         component={DashboardScreen}
         options={{
           headerShown: false,
-          tabBarIcon: () => (
-            <FontAwesome5 name="home" size={30} color="#9c6b45" />
-          ),
+          tabBarIcon:()=>(<Image source={homeIcon} style={{height:40,
+            width:40,resizeMode:"contain",marginVertical:10,}}/>),
           tabBarColor:"#9c6b45",
         }}
       />
@@ -35,8 +35,8 @@ const MainNavigator = () => {
         component={ProfileScreen}
         options={{
            headerShown: false,
-           tabBarIcon:()=><Image source={leaderBoardIcon} style={{height:45,
-          width:45,resizeMode:"contain",marginVertical:10,}}/>
+           tabBarIcon:()=><Image source={leaderBoardIcon} style={{height:50,
+          width:50,resizeMode:"contain",marginVertical:10,}}/>
           //  <FontAwesome name="user" size={30} color="#0096FF" /> 
         
         }}
@@ -46,8 +46,8 @@ const MainNavigator = () => {
         component={ProfileScreen}
         options={{
            headerShown: false,
-           tabBarIcon:()=><Image source={profileIcon} style={{height:35,
-          width:35,resizeMode:"contain",marginVertical:10,}}/>
+           tabBarIcon:()=><Image source={profileIcon} style={{height:40,
+          width:40,resizeMode:"contain",marginVertical:10,}}/>
           //  <FontAwesome name="user" size={30} color="#0096FF" /> 
         
         }}
