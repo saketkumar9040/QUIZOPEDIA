3f159ec23fc7efe81d86fda2dbfb50f30d366c53
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-09-27 02:56:26 +0530

    feat - question screen made

--- src/screens/questionScreen/index.js
+++ src/screens/questionScreen/index.js
@@ -5,17 +5,19 @@ import axios from "axios";
 
 const QuestionScreen = ({ navigation, route }) => {
   const data = route.params;
-  console.log(data);
+//   console.log(data);
 
   const [category,setCategory] = useState("");
   const [difficulty,setDifficulty] = useState("");
   const [amount,setAmount] = useState(10);
+  const [ questions,setQuestions] = useState([])
 
   const fetchData = async () => {
     const data = await axios.get(
       `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=multiple`
     );
-    console.log(data.data.results);
+    // console.log(data.data.results);
+    setQuestions(data.data.results)
   };
 
   useEffect(() => {
@@ -26,8 +28,16 @@ const QuestionScreen = ({ navigation, route }) => {
   }, [data]);
 
   return (
-    <View>
-      <Text>QuestionScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.questionText}>
+          {questions.map((item,index)=>{
+            return(
+                <View key={index}>
+                <Text>{item.question}</Text>
+                </View>
+            );
+          })}
+      </View>
     </View>
   );
 };
