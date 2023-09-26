340b4a0dd1486c24059b8abe2eafec61d4cb1aa7
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-09-26 16:46:17 +0530

    feat - added bottom tab in dashboard

--- src/navigations/MainNavigator.js
+++ src/navigations/MainNavigator.js
@@ -1,14 +1,17 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import DashboardScreen from "../screens/dashboard";
 
-const Stack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const MainNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="dashboard" component={DashboardScreen} options={{headerShown:false}}/>
-    </Stack.Navigator>
+    <Tab.Navigator>
+      <Tab.Screen name="dashboard" component={DashboardScreen} options={{headerShown:false}}/>
+    </Tab.Navigator>
   );
 };
 
 export default MainNavigator;
+
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
--- src/screens/dashboard/style.js
+++ src/screens/dashboard/style.js
@@ -3,17 +3,18 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        alignItems:"center"
+        // alignItems:"center"
     },
     logoContainer:{
         flexDirection:"row",
         alignItems:"center",
-        justifyContent:"center"
+        justifyContent:"center",
+        elevation:10,
     },
     logo:{
         width:300,
         height:300,
-        resizeMode:"contain"
+        resizeMode:"contain",
     }
 });
 
